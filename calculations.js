466c8871babc511fd8c67af9b57657eb46def18e
Marius Beckers <marius@beckers.de>
2021-11-02 16:31:36 +0100

    pushed current state

--- calculations.js
+++ calculations.js
@@ -45,10 +45,48 @@ function render() {
     torches.forEach(torch => {
         var li = document.createElement("li");
         li.appendChild(
-            document.createTextNode(
-                torch.type + ' ' + torch.attribute + ' ' + torch.resistance
-            )
+            renderSingleTorch(torch)
         );
         ul.appendChild(li);
     });
+}
+
+function renderSingleTorch(torch) {
+    var div = document.createElement("div");
+    div.className += " torches";
+    var div1 = document.createElement("div");
+    div1.className += " torch";
+    var span1 = document.createElement("span");
+    span1.appendChild(
+        document.createTextNode(
+            torch.type
+        )
+    );
+    div1.appendChild(span1)
+
+    var div2 = document.createElement("div");
+    div2.className += " torch";
+    var span2 = document.createElement("span");
+    span2.appendChild(
+        document.createTextNode(
+          torch.attribute
+        )
+    );
+    div2.appendChild(span2)
+
+    var div3 = document.createElement("div");
+    div3.className += " torch";
+    var span3 = document.createElement("span");
+    span3.appendChild(
+        document.createTextNode(
+            torch.resistance
+        )
+    );
+    div3.appendChild(span3)
+
+    div.appendChild(div1);
+    div.appendChild(div2);
+    div.appendChild(div3);
+
+    return div;
 }
